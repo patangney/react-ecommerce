dde66e01cbdb09dad57c4d94cb735e30e9b06d19
Patrick Tangney <ptangney@outlook.com>
2022-03-04 12:24:04 +0000

    setup store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,7 +3,7 @@
  * we can catch them and display them, the middleware will recieve actions within and pass to
  * root reducer
  * ie - catch action, log then moves it along
- * 
+ *
  * middleware that are stored by redux is expected to be an array
  */
 
@@ -11,4 +11,9 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from 'redux/root-reducer';
 
+const middlewares = [logger];
 
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+
+export default store;
