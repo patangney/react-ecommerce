f69f3f69693707413fa22aa98904d7cbc6c31ec6
Patrick Tangney <ptangney@outlook.com>
2022-03-04 14:42:47 +0000

    setup cart action types

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,13 +3,15 @@
  * Whatever the boolean value is set as, convert to oposite
  */
 
+import CartActionTypes from './cart.types'
+
 const INITIAL_STATE = {
     hidden: true
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'TOGGLE_CART_HIDDEN': 
+        case CartActionTypes.TOGGLE_CART_HIDDEN: 
             return {
                 ...state,
                 hidden: !state.hidden
