d5c091d914c126377c4167d3aba006ba77acfdc2
Patrick Tangney <ptangney@outlook.com>
2022-03-04 10:42:56 +0000

    create user signup - email and password

--- src/pages/signInSignUp/signInSignUp.component.jsx
+++ src/pages/signInSignUp/signInSignUp.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from 'components/sign-up/signup.Component';
+
 import './_signInSignUp.styles.scss';
 
 
@@ -7,6 +9,7 @@ const SignInSignUpPage = ()  => (
 
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 
 );
