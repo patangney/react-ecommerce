e398128515e44d25aa1a0ab1b0c5ba8ae73fdc10
Patrick Tangney <ptangney@outlook.com>
2022-03-04 12:15:44 +0000

    setup store js - createStore, applyMiddlewares, logger into rootReducer

---
+++ src/redux/store/store.js
@@ -0,0 +1,14 @@
+/**
+ * Add middleware to the store so that whenever actions get fired or dispatched
+ * we can catch them and display them, the middleware will recieve actions within and pass to
+ * root reducer
+ * ie - catch action, log then moves it along
+ * 
+ * middleware that are stored by redux is expected to be an array
+ */
+
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import rootReducer from 'redux/root-reducer';
+
+
