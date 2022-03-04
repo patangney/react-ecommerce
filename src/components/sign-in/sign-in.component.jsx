3fa81dd2bdfd5445fdb91bc55776bcd37cca56db
Patrick Tangney <ptangney@outlook.com>
2022-03-04 11:30:40 +0000

    slight change to customButton - added type

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -66,7 +66,7 @@ class SignIn extends React.Component {
             <CustomButton type='submit' value='Submit Form'>
               Sign In
             </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
           </div>
