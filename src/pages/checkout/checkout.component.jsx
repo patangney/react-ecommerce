a6f9420771259d900eb39b47fb55d4aa938faa6f
Patrick Tangney <ptangney@outlook.com>
2022-03-06 17:54:23 +0000

    setup mapStateToProps and createStructured Selector

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
+import { selectCartItems } from 'redux/cart/cart.selectors';
 
 import './_checkout.styles.scss';
 
@@ -7,9 +10,29 @@ import './_checkout.styles.scss';
 
 const CheckOutPage = ()  => (
 
-    <div> 
-        TEST
+    <div className='checkout-page'> 
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span className=''>Product</span>
+            </div>
+            <div className='header-block'>
+                <span className=''>Description</span>
+            </div>
+            <div className='header-block'>
+                <span className=''>Price</span>
+            </div>
+            <div className='header-block'>
+                <span className=''>Quantity</span>
+            </div>
+            <div className='header-block'>
+                <span className=''>Remove</span>
+            </div>
+        </div>
     </div>
 
 );
-export default CheckOutPage;
+
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
+})
+export default connect(mapStateToProps)(CheckOutPage);
