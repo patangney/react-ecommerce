f1a91814efaf3e8c9e3083a790656cba4a15f545
Patrick Tangney <ptangney@outlook.com>
2022-03-06 14:28:08 +0000

    setup cartItems.reduce() method to map state of items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,25 @@ import './_cart-icon.styles.scss';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shoppingbag.svg';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-export default connect(null,mapDispatchToProps)(CartIcon);
+
+/**
+ * mapStateToProps() - Selector
+ * Gets the whole state object, pulls off a slice of the state and 
+ *  reduce over the items to get a new value
+ * 
+ */
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity, 0)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
