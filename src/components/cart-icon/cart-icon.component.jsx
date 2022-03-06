84323b0911bfdb2eda1743034a0317f6e786193b
Patrick Tangney <ptangney@outlook.com>
2022-03-06 15:19:54 +0000

    updating with cart selectors - redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from 'redux/cart/cart.actions';
+import { selectCartItemsCount } from 'redux/cart/cart.selectors';
 import './_cart-icon.styles.scss';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shoppingbag.svg';
@@ -22,8 +23,8 @@ const mapDispatchToProps = (dispatch) => ({
  *  reduce over the items to get a new value
  * memoize and not re-render a component if the state value does not change by using the reselect library
  */
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce((accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity, 0)
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -5,3 +5,19 @@ import { createSelector } from 'reselect';
  */
 
 const selectCart = (state) => state.cart;
+
+// const selectUser = state => state.user;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity,
+      0
+    )
+);
