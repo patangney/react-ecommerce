83b18fcd58182397a46b4d35cedb188c58187f4b
Patrick Tangney <ptangney@outlook.com>
2022-03-07 13:00:15 +0000

    setup checkout-item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,11 +5,11 @@ import { withRouter } from 'react-router-dom';
 import { selectCartItems } from 'redux/cart/cart.selectors';
 import CustomButton from 'components/buttons/buttons.component';
 import CartItem from 'components/cart-item/cart-item.component';
+import { toggleCartHidden } from 'redux/cart/cart.actions';
+
 import './cart-dropdown.styles.scss';
 
-
-
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -20,7 +20,14 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className='empty-message'>Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')} >Go to Checkout</CustomButton>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden);
+      }}
+    >
+      Go to Checkout
+    </CustomButton>
   </div>
 );
 const mapStateToProps = createStructuredSelector({
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import './_checkout-item.styles.scss';
+
+
+
+const CheckOutItem = ()  => (
+
+    <div> 
+    </div>
+
+);
+export default CheckOutItem;
