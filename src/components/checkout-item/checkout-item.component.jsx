157d0098cd0f39fc03d6a036b3969fd2291bd0a2
Patrick Tangney <ptangney@outlook.com>
2022-03-08 12:05:57 +0000

    setup remove item reducer / cart action

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -11,7 +11,11 @@ const CheckOutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow'>&#10094;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow'>&#10095;</div>
+      </span>
       <span className='price'>{price}</span>
       <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
     </div>
